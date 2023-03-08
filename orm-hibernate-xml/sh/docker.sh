9fe99eb07e78a3e2a6b227d295933dabf472393b
spectre2727 <gsoldano27@outlook.com>
2023-03-08 20:57:52 +0100

    [Project] orm-hibernate-xml

--- orm-hibernate-xml/sh/docker.sh
+++ orm-hibernate-xml/sh/docker.sh
@@ -2,4 +2,4 @@ mvn clean install
 docker rm -f orm-hibernate-xml-container
 docker rmi orm-hibernate-xml-image
 docker build -t orm-hibernate-xml-image .
-docker run --rm --name orm-hibernate-xml-container orm-jpa-xml-image
+docker run --rm --name orm-hibernate-xml-container orm-hibernate-xml-image
